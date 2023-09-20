10a4a9eef123a437eaef182e58b075cdb0f1792b
Igorfrf <igfreitasferreira@gmail.com>
2023-09-20 11:11:29 -0300

    first commit

---
+++ src/Data/notes.js
@@ -0,0 +1,13 @@
+export const notes = [
+  {
+    id: 0,
+    title: "Bom dia",
+    description: "como está?",
+  },
+  {
+    id: 1,
+    title: "boa noite",
+    description: "fazer janta",
+  },
+
+];
